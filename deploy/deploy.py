5b62d55393d8319ec326b43654bff0eab220b6fa
Dennis Terhorst <d.terhorst@fz-juelich.de>
2020-12-09 12:25:05 +0100

    improved dependency graph output

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -2,6 +2,7 @@
 # encoding: utf8
 '''
 Usage: deploy [options] show <config>
+       deploy [options] graph <config>
 
 Options:
     -v, --verbose       increase output
@@ -18,6 +19,19 @@ logging.basicConfig(level=logging.DEBUG)
 from ruamel.yaml.constructor import DuplicateKeyError
 from ruamel.yaml import YAML
 yaml = YAML()
+
+# from https://www.tutorialspoint.com/insertion-sort-in-python-program
+def insertionSort(arr):
+   for i in range(1, len(arr)):
+      key = arr[i]
+      # Move elements of arr[0..i-1], that are greater than key,
+      # to one position ahead of their current position
+      j = i-1
+      while j >=0 and key < arr[j] :
+         arr[j+1] = arr[j]
+         j -= 1
+      arr[j+1] = key
+
 
 class Package:
     def __init__(self, package, version=None, variant="default"):
@@ -92,6 +106,9 @@ class Package:
             return False
         return self._version is None or self._version < other._version
 
+    def __eq__(self, other):
+        return self._package == other._package
+
     def __format__(self, fmt):
         v = self._version or "*"
         text = f'{self._package}/{v}/{self._variant}'
@@ -127,35 +144,33 @@ class Build:
 
     def __eq__(self, other):
         '''
-        returns true if two packages can be built at the same time
-        i.e. no dependencies between the two packages
+        returns true if two packages do not have ordering depencies, i.e.
+        neither depends (even indirectly) on the other.
+
         >>> A = Build("foo")
         >>> B = Build("bar", ["foo"])
-        >>> A == B
-        False
-        >>> B == A
+        >>> A == B or B == A
         False
         >>> C = Build("baz")
-        >>> A == C
-        True
-        >>> C == A
+        >>> A == C and C == A
         True
 
         Also having less specific dependencies causes non-parallel builds
-        #>>> A = Build("foo/1.2.3")
-        #>>> B = Build("bar", ["foo"])
-        #>>> A == B
-        #False
-        #>>> B == A
-        #False
+        >>> A = Build("foo/1.2.3")
+        >>> B = Build("bar", ["foo"])
+        >>> A == B or B == A
+        False
         '''
-        return (self._package == other._package
-               or
-               ( self._package not in other._dependencies
-                 and other._package not in self._dependencies))
+        result = not self < other and not other < self
+        log.debug("compring %s %s %s", self, {True:"==", False:"<>"}[result], other)
+        return result
+        #return (self._package == other._package
+        #       or
+        #       ( self._package not in other._dependencies
+        #         and other._package not in self._dependencies))
 
     def __repr__(self):
-        return f'{self._package}()'
+        return f'{self._package}(%s)' % "; ".join([str(d) for d in self._dependencies])
 
     def __lt__(self, other):
         '''
@@ -170,8 +185,30 @@ class Build:
         >>> C = Build("baz")
         >>> A < C
         False
+
+        Especially, undefined version/variant should always be built before:
+        >>> D = Build("gcc", ['mpc'])
+        >>> E = Build("gsl", ['gcc/10.2.0/default'])
+        >>> D < E
+        True
+        >>> D > E
+        False
+
+        >>> A = Build("gcc", ["mpc"])
+        >>> B = Build('boost', ['gcc/10.2.0'])
+        >>> C = Build('gsl/2.6', ['gcc/10.2.0'])
+        >>> A < B
+        True
+        >>> A < C
+        True
+        >>> B < A
+        False
+        >>> C < A
+        False
         '''
-        return self._package in other._dependencies or any([self._package < dep for dep in other._dependencies])
+        result = self._package in other._dependencies or any([self._package == dep for dep in other._dependencies])
+        log.debug("compring %s %s %s", self, {True:"<", False:">="}[result], other)
+        return result
 
 
 def red(x):
@@ -211,6 +248,93 @@ def CommandsView(deployment):
         output.append(bold(f"{build.package.build_command}"))
     return "\n".join(output) + "\n"
 
+class DotView:
+    def __init__(self):
+        self._default_node_attributes = {}
+        self._default_edge_attributes = {}
+
+    def attr(self, x, asep=', '):
+        '''
+        Return a dot formatted attribute string for the dictionary X
+
+        Parameters
+        ----------
+
+        x : dictionary
+            Key value pairs to be formatted as DOT attributes
+
+        asep : str
+            separator between different attributes
+
+        Returns
+        -------
+
+        str : attributes formatted for DOT files
+
+        Example
+        -------
+
+        >>> dot = DotView()
+        >>> dot.attr({'label': 'boost/*/default'})
+        'label="boost/*/default"'
+        '''
+        return asep.join([f'{k}="{v}"' for k,v in x.items()])
+
+    def __call__(self, graph):
+        name = "g"
+        if "name" in graph:
+            name = graph['name']
+        output = ["digraph %s {" % name]
+
+        output.append("    " + self.attr(graph.graph, asep="\n    "))
+        output.append("\n    node [%s];" % self.attr(self._default_node_attributes))
+        for node, data in graph.nodes(data=True):
+            log.debug("node: %s (%s)", node, data)
+        output.extend(['    {} [{}];'.format(n, self.attr(nattr)) for n, nattr in graph.nodes(data=True)])
+
+
+        output.append("\n    edges [%s];" % self.attr(self._default_edge_attributes))
+        for root in [node for node, degree in graph.in_degree() if degree == 0]:
+            output.append("    // from %s" % graph.nodes[root]["label"])
+            output.extend(['    {} -> {};'.format(*e) for e in networkx.algorithms.dfs_tree(graph, source=root).edges])
+        output.append("}")
+        return "\n".join(output)
+
+
+import networkx
+def GraphPresenter(deployment):
+    '''
+    Presents the build dependencies as a NetworkX graph object.
+
+    Parameters
+    ----------
+
+    deployment : Deployment
+
+        filled Deployment object.
+
+    Returns
+    -------
+
+    networkx.DiGraph : dependency tree of all Builds in the deployment
+    '''
+    g = networkx.DiGraph(name="deps", title="Build Dependencies")
+    nodeids = {}
+    def nodeid(x):
+        return nodeids.setdefault(repr(x), f"node{len(nodeids)}")
+
+    def makenode(package):
+        return (nodeid(package), {"label": repr(package)})
+
+    g.add_nodes_from([makenode(build.package) for build in deployment._builds])
+
+    for build in deployment._builds:
+        g.add_nodes_from([makenode(dep) for dep in build.dependencies])     # required for non-built packages
+        g.add_edges_from([(nodeid(build.package), nodeid(dep)) for dep in build.dependencies])
+
+    log.info("Graph info:\n%s", networkx.classes.function.info(g))
+    return g
+
 
 class Deployment:
     '''
@@ -222,7 +346,7 @@ class Deployment:
     ...        Build("C", ["D"])
     ...     ])
     >>> list(d)
-    [C/*/default(), B/*/default(), A/*/default()]
+    [C/*/default(D/*/default), B/*/default(C/*/default), A/*/default(B/*/default)]
 
     circular dependencies will raise an error
     >>> d = Deployment([
@@ -253,8 +377,11 @@ class Deployment:
 
         '''
         done = []
-        for build in sorted(self._builds):
+        for build in sorted(list(self._builds)):
             if any([build.package in donepkg.dependencies for donepkg in done]):
+                log.error("building %s, but it has already been used by\n%s",
+                          build,
+                          [str(donepkg) for donepkg in done if build.package in donepkg.dependencies])
                 raise ValueError("package %s seems to have a cyclic dependency!" % build.package)
             yield build
             done.append(build)
@@ -269,7 +396,7 @@ class Deployment:
         >>> B.package in D
         True
         >>> print(D._builds)
-        [foo/*/default(), bar/*/default()]
+        [foo/*/default(), bar/*/default(foo/*/default; baz/*/default)]
         >>> Package("baz") in D
         False
         '''
@@ -284,6 +411,34 @@ def read_config(filename):
     except DuplicateKeyError as e:
         log.error("%s", e)
         exit(2)
+
+
+# package > dependency:
+# 02 nest-simulator/2.18.0/default(gsl/2.6/default; boost/1.74.0/default; cmake/*/default; mpi/*/default) >= cmake/*/default()
+# 15 gcc/*/default(mpc/*/default) >= mpfr/*/default(gmp/*/default)
+# 16 gcc/*/default(mpc/*/default) >= mpc/*/default(mpfr/*/default)
+#
+# package <> package:
+# 01 cmake/*/default() >= boost/*/default(gcc/10.2.0/default)
+# 03 nest-simulator/2.20.0/default(gsl/*/default; boost/1.74.0/default; cmake/*/default; mpi/*/default) >= nest-simulator/2.18.0/default(gsl/2.6/default; boost/1.74.0/default; cmake/*/default; mpi/*/default)
+# 06 gsl/2.6/default(gcc/10.2.0/default) >= cmake/*/default()
+#
+# dependency < package (correct):
+# 04 gsl/2.6/default(gcc/10.2.0/default) < nest-simulator/2.20.0/default(gsl/*/default; boost/1.74.0/default; cmake/*/default; mpi/*/default)
+# 05 gsl/2.6/default(gcc/10.2.0/default) < nest-simulator/2.18.0/default(gsl/2.6/default; boost/1.74.0/default; cmake/*/default; mpi/*/default)
+#
+# dependency < package (wrong!):
+# 07 gmp/*/default() >= gsl/2.6/default(gcc/10.2.0/default)
+# 08 gmp/*/default() >= nest-simulator/2.20.0/default(gsl/*/default; boost/1.74.0/default; cmake/*/default; mpi/*/default)
+# 09 mpfr/*/default(gmp/*/default) >= nest-simulator/2.18.0/default(gsl/2.6/default; boost/1.74.0/default; cmake/*/default; mpi/*/default)
+# 10 mpfr/*/default(gmp/*/default) >= gmp/*/default()
+# 11 mpc/*/default(mpfr/*/default) >= nest-simulator/2.18.0/default(gsl/2.6/default; boost/1.74.0/default; cmake/*/default; mpi/*/default)
+# 12 mpc/*/default(mpfr/*/default) >= gmp/*/default()
+# 13 mpc/*/default(mpfr/*/default) >= mpfr/*/default(gmp/*/default)
+# 14 gcc/*/default(mpc/*/default) >= nest-simulator/2.20.0/default(gsl/*/default; boost/1.74.0/default; cmake/*/default; mpi/*/default)
+#
+# ERROR:root:building gcc/*/default(mpc/*/default), but it has already been used by
+# ['boost/*/default(gcc/10.2.0/default)', 'gsl/2.6/default(gcc/10.2.0/default)']
 
 
 def main():
@@ -301,8 +456,10 @@ def main():
         plan.append(Build(package, dependencies))
 
     if args['show']:
-        plan.show()
-
+        print(CommandsView(plan))
+    elif args['graph']:
+        dot = DotView()
+        print(dot(GraphPresenter(plan)))
 
 if __name__ == '__main__':
     main()
