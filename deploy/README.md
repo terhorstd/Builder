c1057da1a315305cf62a7d2d368d2ef1384bf4b2
Dennis Terhorst <d.terhorst@fz-juelich.de>
2020-12-09 19:47:05 +0100

    first fully featured deploy.py version

--- deploy/README.md
+++ deploy/README.md
@@ -1,5 +1,78 @@
 
-These scripts help to build mutiple packages with builder. This complements the
-dependency-less building process, by allowing to configure exact dependencies
-for the target system.
+
+Deploy
+------
+
+Deploy handles the organization of many build commands for a specific site when
+using the Builder tool. As usually the Builds require prior loading of modules,
+and builds may be done in different variants, the complete software stack
+becomes difficult to track. Deploy manages a set of defined builds and can
+provide organizational overviews.
+
+This complements the dependency-less building process, by allowing to configure
+exact dependencies for the target system. Dependency resolution outside of the
+user-provided build configuration is left to the system, enabling maximum use
+of system provided resources (e.g. in HPC environments).
+
+
+Usage
+-----
+
+Deploy is based on a YAML input file specifying the builds to be executed by
+Builder. The format is a simple dictionary of lists, where keys and values are
+package strings. Each entry is a Build and required loaded modules.
+
+```yaml
+boost:
+- gcc
+
+cooltool:
+- gcc
+- boost
+```
+
+This file can be given as site config to `deploy`. To see available commands
+run
+
+```bash
+deploy.py --help
+```
+
+The `show` subcommand is used to print all commands necessary for the full
+deployment. You can inspect what would be done, or send output to a file and
+use it as a deployment script.
+
+```bash
+deploy.py show site.config >build_all.sh
+bash build_all.sh
+```
+
+Note that the actual build settings are configured with Builder's `.buildrc`.
+See documentation of Builder for details.
+
+
+Testing
+-------
+
+   Run `pytest` to check package works as expected.
+
+
+License
+-------
+
+   deploy – build automation system
+   Copyright (C) 2020  Dennis Terhorst
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
--- deploy/deploy.py
+++ deploy/deploy.py
@@ -1,47 +1,97 @@
 #!/usr/bin/env python
 # encoding: utf8
+#
+#   deploy.py – build automation system
+#   Copyright (C) 2020  Dennis Terhorst
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 '''
 Usage: deploy [options] show <config>
        deploy [options] graph <config>
+
+  Deploy handles the organization of many build commands for a specific site
+  when using the Builder tool.  As usually the Builds require prior loading of
+  modules, and builds may be done in different variants, the complete software
+  stack becomes difficult to track. Deploy manages a set of defined builds and
+  can provide organizational overviews.
+
+Subcommands are
+
+  show
+     print the commands of all builds ordered by their dependencies.
+
+  graph
+    Print a DOT graph of the package dependencies to stdout. The resulting
+    graph can be converted with Graphviz, for example:
+        deploy.py graph site.config | dot -Tx11
 
 Options:
     -v, --verbose       increase output
     -h, --help          print this text
 '''
 from docopt import docopt
-from sys import exit
+from sys import exit, stdout
 from functools import total_ordering
 from pprint import pformat
 from distutils.version import LooseVersion as Version
+import networkx
+from networkx.classes.function import degree as nx_degree, info as nx_info
+from networkx.algorithms import dfs_tree as nx_dfs_tree
+from networkx.algorithms.dag import topological_sort as nx_topological_sort
 import logging
-log = logging.getLogger()
-logging.basicConfig(level=logging.DEBUG)
 from ruamel.yaml.constructor import DuplicateKeyError
 from ruamel.yaml import YAML
-yaml = YAML()
 
-# from https://www.tutorialspoint.com/insertion-sort-in-python-program
-def insertionSort(arr):
-   for i in range(1, len(arr)):
-      key = arr[i]
-      # Move elements of arr[0..i-1], that are greater than key,
-      # to one position ahead of their current position
-      j = i-1
-      while j >=0 and key < arr[j] :
-         arr[j+1] = arr[j]
-         j -= 1
-      arr[j+1] = key
+yaml = YAML()
+log = logging.getLogger()
+logging.basicConfig(level=logging.DEBUG)
 
 
 class Package:
+    '''
+    Representation of a single software package.
+
+    Packages have a name, version and variant, where version and variant
+    default to None and "default" respectively.
+
+    >>> Package("A")
+    A/*/default
+
+    Packages support pythonic operations:
+
+    >>> l = [Package("foo")]
+    >>> Package("foo/1.2") in l
+    False
+    >>> l = [Package("foo")]
+    >>> Package("foo") in l
+    True
+    '''
     def __init__(self, package, version=None, variant="default"):
         '''
+        Initialize the Package instance either from explicitly separate values
+        or from a combined package string.
+
+        Explicitly:
         >>> Package("A")
         A/*/default
         >>> Package("B", "0.1")
         B/0.1/default
         >>> Package("c", "0.2", "testing")
         c/0.2/testing
+
+        By package strings:
         >>> Package("d/0.3.1")
         d/0.3.1/default
         >>> Package("d/0.4/foo")
@@ -61,7 +111,7 @@ class Package:
         if "/" in package:
             if version is not None:
                 raise ValueError("can not have version in package and version arugments")
-            pvv = package.split('/',3)
+            pvv = package.split('/', 3)
             if pvv:
                 self._package = pvv.pop(0)
             if pvv:
@@ -82,10 +132,9 @@ class Package:
         return self._variant
 
     def __eq__(self, other):
-        v = self._version
-        if self._version is None:
-            v = other._version
-        return (self._package == other._package and v == other._version and self._variant == other.variant)
+        return (self._package == other._package
+                and self._version == other._version
+                and self._variant == other.variant)
 
     def __lt__(self, other):
         '''
@@ -106,9 +155,6 @@ class Package:
             return False
         return self._version is None or self._version < other._version
 
-    def __eq__(self, other):
-        return self._package == other._package
-
     def __format__(self, fmt):
         v = self._version or "*"
         text = f'{self._package}/{v}/{self._variant}'
@@ -119,6 +165,14 @@ class Package:
         return f'{self._package}/{v}/{self._variant}'
 
     @property
+    def load_command(self):
+        if self._version is None:
+            return f"module load {self._package}"
+        if self._variant != "default":
+            return f"module load {self._package}/{self._version}/{self._variant}"
+        return f"module load {self._package}/{self._version}"
+
+    @property
     def build_command(self):
         if self._version is None:
             return f"build {self._package}"
@@ -127,8 +181,6 @@ class Package:
         return f"build {self._package} {self._version}"
 
 
-
-@total_ordering
 class Build:
     def __init__(self, package, dependencies=[]):
         self._package = Package(package)
@@ -142,116 +194,91 @@ class Build:
     def dependencies(self):
         yield from self._dependencies
 
-    def __eq__(self, other):
-        '''
-        returns true if two packages do not have ordering depencies, i.e.
-        neither depends (even indirectly) on the other.
-
-        >>> A = Build("foo")
-        >>> B = Build("bar", ["foo"])
-        >>> A == B or B == A
-        False
-        >>> C = Build("baz")
-        >>> A == C and C == A
-        True
-
-        Also having less specific dependencies causes non-parallel builds
-        >>> A = Build("foo/1.2.3")
-        >>> B = Build("bar", ["foo"])
-        >>> A == B or B == A
-        False
-        '''
-        result = not self < other and not other < self
-        log.debug("compring %s %s %s", self, {True:"==", False:"<>"}[result], other)
-        return result
-        #return (self._package == other._package
-        #       or
-        #       ( self._package not in other._dependencies
-        #         and other._package not in self._dependencies))
-
     def __repr__(self):
         return f'{self._package}(%s)' % "; ".join([str(d) for d in self._dependencies])
-
-    def __lt__(self, other):
-        '''
-        returns true if this build has to run before the other
-
-        >>> A = Build("foo")
-        >>> B = Build("bar", ["foo"])
-        >>> A < B
-        True
-        >>> B < A
-        False
-        >>> C = Build("baz")
-        >>> A < C
-        False
-
-        Especially, undefined version/variant should always be built before:
-        >>> D = Build("gcc", ['mpc'])
-        >>> E = Build("gsl", ['gcc/10.2.0/default'])
-        >>> D < E
-        True
-        >>> D > E
-        False
-
-        >>> A = Build("gcc", ["mpc"])
-        >>> B = Build('boost', ['gcc/10.2.0'])
-        >>> C = Build('gsl/2.6', ['gcc/10.2.0'])
-        >>> A < B
-        True
-        >>> A < C
-        True
-        >>> B < A
-        False
-        >>> C < A
-        False
-        '''
-        result = self._package in other._dependencies or any([self._package == dep for dep in other._dependencies])
-        log.debug("compring %s %s %s", self, {True:"<", False:">="}[result], other)
-        return result
 
 
 def red(x):
     return f"\033[31m{x}\033[m"
 
+
 def green(x):
     return f"\033[32m{x}\033[m"
+
 
 def blue(x):
     return f"\033[34m{x}\033[m"
 
+
 def orange(x):
-    return f"\033[00;39m{x}\033[m"
+    return f"\033[00;33m{x}\033[m"
+
 
 def yellow(x):
     return f"\033[00;33m{x}\033[m"
 
+
 def gray(x):
     return f"\033[00;90m{x}\033[m"
+
 
 def bold(x):
     return f"\033[01m{x}\033[m"
 
-def CommandsView(deployment):
-    output = list()
-    done = []
-    for build in deployment:
-        done_already = []
+
+if not stdout.isatty():
+    log.info("output to a non-tty.")
+    nocolor = lambda x: x
+    red = nocolor
+    green = nocolor
+    blue = nocolor
+    orange = nocolor
+    yellow = nocolor
+    gray = nocolor
+    bold = nocolor
+
+
+class CommandsView:
+
+    def __call__(self, graph):
+        output = list()
+        donepkgs = []
+        assert isinstance(graph, networkx.DiGraph)
+        for node in nx_topological_sort(graph):
+            subgraph = graph[node]
+            data = graph.nodes[node]
+            if isinstance(data['item'], Build):
+                build = data['item']
+                output.extend(self._template(build, donepkgs))
+                donepkgs.append(build.package)
+
+        return "\n".join(output) + "\n"
+
+    def _template(self, build, donepkgs=[]):
+        '''
+        Write a commands section for the given build
+        '''
+        output = []
         output.append(blue(f"\n# Building {build.package}"))
         output.append(gray("module purge"))
         for dep in build.dependencies:
             comment = blue("# ")
             info = gray("system provided")
-            if dep in deployment:
-                info = green("rebuilt")
-            output.append(f"module load {dep:30s}{comment}{info}")
+            assert isinstance(dep, Package)
+            if dep in donepkgs:
+                info = green("just rebuilt")
+            elif any([dep.name == done.name for done in donepkgs]):
+                info = orange("maybe rebuilt")
+            output.append(f"{dep.load_command:40s}{comment}{info}")
         output.append(bold(f"{build.package.build_command}"))
-    return "\n".join(output) + "\n"
+        return output
+
 
 class DotView:
     def __init__(self):
         self._default_node_attributes = {}
         self._default_edge_attributes = {}
+        self._label_format = "{pkg[label]}"
 
     def attr(self, x, asep=', '):
         '''
@@ -278,10 +305,20 @@ class DotView:
         >>> dot.attr({'label': 'boost/*/default'})
         'label="boost/*/default"'
         '''
-        return asep.join([f'{k}="{v}"' for k,v in x.items()])
+        return asep.join([f'{k}="{v}"' for k, v in x.items()])
+
+    def node_attr(self, node, attr):
+        'returns the graphviz node properties, given a package'
+        style = {
+            'label': self._label_format.format(pkg=attr),
+        }
+        if attr.get('type', "") == 'dep':
+            style['style'] = 'dashed'
+        return self.attr(style)
 
     def __call__(self, graph):
         name = "g"
+        graph.reverse(copy=False)
         if "name" in graph:
             name = graph['name']
 
@@ -290,21 +327,20 @@ class DotView:
         output.append("    " + self.attr(graph.graph, asep="\n    "))
 
         # nodes
-        for node, data in graph.nodes(data=True):       # debug only
-            log.debug("node: %d %s (%s)", networkx.classes.function.degree(graph, node), node, data)
+        # for node, data in graph.nodes(data=True):       # debug only
+        #    log.debug("node: %d %s (%s)", nx_degree(graph, node), node, data)
         output.append("\n    node [%s];" % self.attr(self._default_node_attributes))
-        output.extend(['    {} [{}];'.format(n, self.attr(nattr)) for n, nattr in graph.nodes(data=True)])
+        output.extend(['    {} [{}];'.format(n, self.node_attr(n, nattr)) for n, nattr in graph.nodes(data=True)])
 
         # edges
-        output.append("\n    edges [%s];" % self.attr(self._default_edge_attributes))
+        output.append("\n    edge [%s];" % self.attr(self._default_edge_attributes))
         for root in [node for node, degree in graph.in_degree() if degree == 0]:
             output.append("    // from %s" % graph.nodes[root]["label"])
-            output.extend(['    {} -> {};'.format(*e) for e in networkx.algorithms.dfs_tree(graph, source=root).edges])
+            output.extend(['    {} -> {};'.format(*e) for e in nx_dfs_tree(graph, source=root).edges])
         output.append("}")
         return "\n".join(output)
 
 
-import networkx
 def GraphPresenter(deployment):
     '''
     Presents the build dependencies as a NetworkX graph object.
@@ -321,22 +357,49 @@ def GraphPresenter(deployment):
 
     networkx.DiGraph : dependency tree of all Builds in the deployment
     '''
-    g = networkx.DiGraph(name="deps", title="Build Dependencies")
     nodeids = {}
+
     def nodeid(x):
         return nodeids.setdefault(repr(x), f"node{len(nodeids)}")
 
-    def makenode(package):
-        return (nodeid(package), {"label": repr(package)})
+    def makenode(package, **attr):
+        attributes = {
+            "label": repr(package),
+            "name": package.name,
+            "version": package.version,
+            "variant": package.variant,
+        }
+        attributes.update(attr)
+        return (nodeid(package), attributes)
 
-    g.add_nodes_from([makenode(build.package) for build in deployment._builds])
+    # graph
+    g = networkx.DiGraph(name="deps", title="Build Dependencies")
 
+    # add `build.package`s
+    g.add_nodes_from([makenode(build.package, type="built", item=build) for build in deployment._builds])
+
+    # add dependant packages and corresponding edges
     for build in deployment._builds:
-        g.add_nodes_from([makenode(dep) for dep in build.dependencies])     # required for non-built packages
+        # required for non-built packages
+        g.add_nodes_from([makenode(dep, type="dep", item=dep)
+                          for dep in build.dependencies if not nodeid(dep) in g.nodes])
         g.add_edges_from([(nodeid(build.package), nodeid(dep)) for dep in build.dependencies])
 
-    log.info("Graph info:\n%s", networkx.classes.function.info(g))
-    return g
+    # add links between wildcard and special packages
+    def nodes_with(key, value, graph):
+        for node, data in g.nodes(data=True):
+            if data[key] != value:
+                continue
+            yield node
+
+    for wildnode in nodes_with('version', None, g):
+        for specnode in nodes_with('name', g.nodes[wildnode]['name'], g):
+            if g.nodes[specnode]['version'] is None:
+                continue
+            g.add_edge(specnode, wildnode, type="induced")
+
+    log.info("Graph info:\n%s", nx_info(g))
+    return g.reverse(copy=False)
 
 
 class Deployment:
@@ -348,46 +411,14 @@ class Deployment:
     ...        Build("B", ["C"]),
     ...        Build("C", ["D"])
     ...     ])
-    >>> list(d)
-    [C/*/default(D/*/default), B/*/default(C/*/default), A/*/default(B/*/default)]
 
-    circular dependencies will raise an error
-    >>> d = Deployment([
-    ...        Build("A", ["B"]),
-    ...        Build("B", ["C"]),
-    ...        Build("C", ["A"])
-    ...     ])
-    >>> list(d)
-    Traceback (most recent call last):
-       ...
-    ValueError: package A/*/default seems to have a cyclic dependency!
     '''
     def __init__(self, builds=[]):
         self._builds = builds
 
-    def show(self):
-        print(CommandsView(self))
-
     def append(self, build):
         assert isinstance(build, Build), "Deployments can only hold Build objects, not %s" % type(build)
         self._builds.append(build)
-
-    def __iter__(self):
-        '''
-        odered iterator
-        raises ValueError if packages are not oderable
-
-
-        '''
-        done = []
-        for build in sorted(list(self._builds)):
-            if any([build.package in donepkg.dependencies for donepkg in done]):
-                log.error("building %s, but it has already been used by\n%s",
-                          build,
-                          [str(donepkg) for donepkg in done if build.package in donepkg.dependencies])
-                raise ValueError("package %s seems to have a cyclic dependency!" % build.package)
-            yield build
-            done.append(build)
 
     def __contains__(self, package):
         '''
@@ -416,41 +447,13 @@ def read_config(filename):
         exit(2)
 
 
-# package > dependency:
-# 02 nest-simulator/2.18.0/default(gsl/2.6/default; boost/1.74.0/default; cmake/*/default; mpi/*/default) >= cmake/*/default()
-# 15 gcc/*/default(mpc/*/default) >= mpfr/*/default(gmp/*/default)
-# 16 gcc/*/default(mpc/*/default) >= mpc/*/default(mpfr/*/default)
-#
-# package <> package:
-# 01 cmake/*/default() >= boost/*/default(gcc/10.2.0/default)
-# 03 nest-simulator/2.20.0/default(gsl/*/default; boost/1.74.0/default; cmake/*/default; mpi/*/default) >= nest-simulator/2.18.0/default(gsl/2.6/default; boost/1.74.0/default; cmake/*/default; mpi/*/default)
-# 06 gsl/2.6/default(gcc/10.2.0/default) >= cmake/*/default()
-#
-# dependency < package (correct):
-# 04 gsl/2.6/default(gcc/10.2.0/default) < nest-simulator/2.20.0/default(gsl/*/default; boost/1.74.0/default; cmake/*/default; mpi/*/default)
-# 05 gsl/2.6/default(gcc/10.2.0/default) < nest-simulator/2.18.0/default(gsl/2.6/default; boost/1.74.0/default; cmake/*/default; mpi/*/default)
-#
-# dependency < package (wrong!):
-# 07 gmp/*/default() >= gsl/2.6/default(gcc/10.2.0/default)
-# 08 gmp/*/default() >= nest-simulator/2.20.0/default(gsl/*/default; boost/1.74.0/default; cmake/*/default; mpi/*/default)
-# 09 mpfr/*/default(gmp/*/default) >= nest-simulator/2.18.0/default(gsl/2.6/default; boost/1.74.0/default; cmake/*/default; mpi/*/default)
-# 10 mpfr/*/default(gmp/*/default) >= gmp/*/default()
-# 11 mpc/*/default(mpfr/*/default) >= nest-simulator/2.18.0/default(gsl/2.6/default; boost/1.74.0/default; cmake/*/default; mpi/*/default)
-# 12 mpc/*/default(mpfr/*/default) >= gmp/*/default()
-# 13 mpc/*/default(mpfr/*/default) >= mpfr/*/default(gmp/*/default)
-# 14 gcc/*/default(mpc/*/default) >= nest-simulator/2.20.0/default(gsl/*/default; boost/1.74.0/default; cmake/*/default; mpi/*/default)
-#
-# ERROR:root:building gcc/*/default(mpc/*/default), but it has already been used by
-# ['boost/*/default(gcc/10.2.0/default)', 'gsl/2.6/default(gcc/10.2.0/default)']
-
-
 def main():
     args = docopt(__doc__)
     if args['--verbose']:
         log.setLevel(logging.DEBUG)
     log.debug(pformat(args))
 
-    log.info("Hello World")
+    log.info("deploy.py (Dennis Terhorst, GPLv3)")
 
     config = read_config(args['<config>'])
 
@@ -459,10 +462,12 @@ def main():
         plan.append(Build(package, dependencies))
 
     if args['show']:
-        print(CommandsView(plan))
+        commands = CommandsView()
+        print(commands(GraphPresenter(plan)))
     elif args['graph']:
         dot = DotView()
         print(dot(GraphPresenter(plan)))
+
 
 if __name__ == '__main__':
     main()
